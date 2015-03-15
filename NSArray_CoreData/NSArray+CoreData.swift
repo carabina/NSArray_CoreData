9608b2f89959d7f940fcb081e1cee30566fc3a50
nofelmahmood <nofelmehmood@gmail.com>
2015-03-15 12:42:34 +0500

    Added Methods for Max Function

--- NSArray_CoreData/NSArray+CoreData.swift
+++ NSArray_CoreData/NSArray+CoreData.swift
@@ -8,41 +8,75 @@
 
 import Foundation
 import CoreData
+import ObjectiveC
 
-extension NSArray {
+extension NSArray{
     
-    class func arrayWithManagedObjects(entityName:String,context:NSManagedObjectContext)->NSArray
+    @objc public class func arrayWithManagedObjects(entityName:String,context:NSManagedObjectContext,error:NSErrorPointer)->NSArray
     {
-        var error:NSError?
         var fetchRequest=NSFetchRequest(entityName: entityName)
-        return context.executeFetchRequest(fetchRequest, error: &error)!
+        var results=context.executeFetchRequest(fetchRequest, error: error)!
+        return results
     }
     
-    class func arrayWithManagedObjects(entityName:String,predicate:NSPredicate,context:NSManagedObjectContext)->NSArray
+    @objc public class func arrayWithManagedObjects(entityName:String,predicate:NSPredicate,context:NSManagedObjectContext,error:NSErrorPointer)->NSArray
     {
-        var error:NSError?
         var fetchRequest=NSFetchRequest(entityName: entityName)
         fetchRequest.predicate=predicate
-        return context.executeFetchRequest(fetchRequest, error: &error)!
+        return context.executeFetchRequest(fetchRequest, error: error)!
     }
-    class func arrayWithManagedObjects(entityName:String,predicate:NSPredicate,range:NSRange,context:NSManagedObjectContext)->NSArray
+    @objc public class func arrayWithManagedObjects(entityName:String,predicate:NSPredicate,range:NSRange,context:NSManagedObjectContext,error:NSErrorPointer)->NSArray
     {
-        var error:NSError?
         var fetchRequest=NSFetchRequest(entityName: entityName)
         fetchRequest.predicate=predicate
         fetchRequest.fetchOffset=range.location
         fetchRequest.fetchLimit=range.length
-        return context.executeFetchRequest(fetchRequest, error: &error)!
+        return context.executeFetchRequest(fetchRequest, error: error)!
     }
-    class func arrayWithManagedObjects(entityName:String,predicate:NSPredicate,range:NSRange,sortDescriptors:NSArray,context:NSManagedObjectContext)->NSArray
+    @objc public class func arrayWithManagedObjects(entityName:String,predicate:NSPredicate,range:NSRange,sortDictionary:NSDictionary,context:NSManagedObjectContext,error:NSErrorPointer)->NSArray
     {
-        var error:NSError?
         var fetchRequest=NSFetchRequest(entityName: entityName)
         fetchRequest.predicate=predicate
         fetchRequest.fetchOffset=range.location
         fetchRequest.fetchLimit=range.length
-        fetchRequest.sortDescriptors=sortDescriptors
-        return context.executeFetchRequest(fetchRequest, error: &error)!
+        return context.executeFetchRequest(fetchRequest, error: error)!
     }
+    @objc public class func arrayWithManagedObjects(entityName:String,predicate:NSPredicate)
+    {
+        var fetchRequest=NSFetchRequest(entityName: entityName)
+        fetchRequest.resultType=NSFetchRequestResultType.DictionaryResultType
+    }
+    @objc public class func arrayWithManagedObjectsByFindingMax(entityName:String,propertyKeyPath:String,resultMaxValueKey:String,resultType:NSAttributeType,context:NSManagedObjectContext,error:NSErrorPointer)->NSArray
+    {
+        var fetchRequest=NSFetchRequest(entityName: entityName)
+        fetchRequest.resultType=NSFetchRequestResultType.DictionaryResultType
+        var keyPathExpression=NSExpression(forKeyPath: propertyKeyPath)
+        var maxExpression=NSExpression(forFunction: "max:", arguments: NSArray(object: keyPathExpression))
+        var expressionDescription=NSExpressionDescription()
+        expressionDescription.name=resultMaxValueKey
+        expressionDescription.expression=maxExpression
+        expressionDescription.expressionResultType=resultType
+        fetchRequest.propertiesToFetch=NSArray(object: expressionDescription)
+        
+        return context.executeFetchRequest(fetchRequest, error: error)!
+    }
+    
+    @objc public class func arrayWithManagedObjectsByFindingMax(entityName:String,predicate:NSPredicate,propertyKeyPath:String,resultMaxValueKey:String,resultType:NSAttributeType,context:NSManagedObjectContext,error:NSErrorPointer)->NSArray
+    {
+        var fetchRequest=NSFetchRequest(entityName: entityName)
+        fetchRequest.resultType=NSFetchRequestResultType.DictionaryResultType
+        fetchRequest.predicate=predicate
+        var keyPathExpression=NSExpression(forKeyPath: propertyKeyPath)
+        var maxExpression=NSExpression(forFunction: "max:", arguments: NSArray(object: keyPathExpression))
+        var expressionDescription=NSExpressionDescription()
+        expressionDescription.name=resultMaxValueKey
+        expressionDescription.expression=maxExpression
+        expressionDescription.expressionResultType=resultType
+        fetchRequest.propertiesToFetch=NSArray(object: expressionDescription)
+        
+        return context.executeFetchRequest(fetchRequest, error: error)!
+    }
+    
+    
 }
 
